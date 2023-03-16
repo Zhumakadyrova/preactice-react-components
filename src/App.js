99b2809371d7dd7007097d28f60cc79e1471312f
Zhumakadyrova <113587900+Zhumakadyrova@users.noreply.github.com>
2023-03-16 14:51:43 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import List from "./List";
+import "./List.css";
+import RedButton from "./RedButton";
+import "./RedButton.css"
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <RedButton/>
+      <List/>
+    </div>
+  );
+}
+export default App;
---
+++ src/RedButton.js
@@ -0,0 +1,8 @@
+function RedButton() {
+  return (
+    <div className="RedButton">
+      <button>Red button</button>
+    </div>
+  );
+}
+export default RedButton;
